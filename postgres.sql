4f0076e5e46e6ae593ed0f3d55a586899b635095
jonas99Dev <jonas@yohrs.com>
2024-11-08 13:44:38 +0100

    rättat tabellbugg

--- postgres.sql
+++ postgres.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Company (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );

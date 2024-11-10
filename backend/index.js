e13aa75cc76d000edfb8611758bdb77ff8d8f285
jonas99Dev <jonas@yohrs.com>
2024-11-10 23:05:43 +0100

    kopplat backend och databas

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 // diverse importer
 const path = require("path");
 const dotenv = require("dotenv");
+dotenv.config(); // Ladda miljövariabler från .env-filen
+
 const express = require("express");
 const cors = require("cors");
 const dummy = require("./dummy.json");
 const customers = require("./customer.json");
+const { Pool } = require("pg");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.PGURI,
+});
 
 // edpoints
 
@@ -33,9 +40,59 @@ app.post("/api/subscribers", (req, res) => {
 });
 
 // /api/customers - GET
-app.get("/api/customers", (req, res) => {
+app.get("/api/customers", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM customer");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching customers:", error);
+    res.status(500).json({ error: "Error fetching customers" });
+  }
   console.log(customers);
-  res.json(customers);
+});
+
+// /api/customers - POST
+app.post("/api/customers", async (req, res) => {
+  const { company_id, name, phone, address, email } = req.body;
+  try {
+    const result = await pool.query(
+      " INSERT INTO customer (company_id, name, phone, address, email) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [company_id, name, phone, address, email]
+    );
+    res.status(201).json(result.rows[0]);
+
+    // res.json(result.rows);
+  } catch (error) {
+    console.error("Error inserting customer:", error);
+    res.status(500).json({ error: "Error inserting customer" });
+  }
+  console.log(customers);
+});
+
+// hämta alla företag från Company-tabellen
+app.get("/api/companies", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM company");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching companies:", error);
+    res.status(500).json({ error: "Error fetching companies" });
+  }
+});
+
+// Skapa en ny post i Company-tabellen
+app.post("/api/companies", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO company (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, password]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error("Error inserting company:", error);
+    res.status(500).json({ error: "Error inserting company" });
+  }
 });
 
 app.listen(port, () => {
